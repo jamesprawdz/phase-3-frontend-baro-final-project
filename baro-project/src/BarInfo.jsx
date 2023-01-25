7cd54975a9a78b824ce3588e943318cfa1ad2682
Avinash Ramsumair <avi@pure235games.com>
2023-01-25 01:58:29 -0500

    update 02

--- baro-project/src/BarInfo.jsx
+++ baro-project/src/BarInfo.jsx
@@ -1,12 +1,65 @@
+import { useState,  useEffect } from "react"
 import { useNavigate } from "react-router-dom";
 
-export default function BarInfo(){
+export default function BarInfo({clickedBar}){
     const navigate = useNavigate()
+    const [reviewArray, setReviewArray] = useState([])
 
+    //fetch review list
+    const fetchReviews = async () => {
+        const req = await fetch('http://localhost:9292/reviews')
+        const res = await req.json()
+        setReviewArray(res)
+    }
+    useEffect(() => {
+        fetchReviews()
+    }, [])
+    
+    const filteredReviewArray = reviewArray.filter((review) =>{
+        return clickedBar.id === review.bar_id
+    })
+    
+    console.log(filteredReviewArray)  
     return(
         <div>
+            {/* Test info and buttons */}
             <h1>Bar Info</h1>
             <button type="button" onClick={() => navigate('/home')}> Home</button>
+
+            <h1 className="bar-info-name">{clickedBar.name}</h1>
+            <img className="bar-info-name" src={clickedBar.image} alt={clickedBar.name}/>
+            <h2 className="bar-info-rating">{clickedBar.rating}</h2>
+            <h2 className="bar-info-category">{clickedBar.category}</h2>
+            <h2 className="bar-info-location">{clickedBar.location}</h2>
+            <h2 className="bar-info-price">{clickedBar.price}</h2>
+            <h2 className="bar-info-closing-time">{clickedBar.closing_time}</h2>
+
+
+            <div className="bar-reivew-container">
+                {filteredReviewArray.map((review) => {
+                    return (
+                        <BarReviewCard
+                            key={review.user_id}
+                            review={review}
+                        />
+                    )
+                })}
+            </div>
+
+        </div>
+    )
+}
+
+
+
+
+function BarReviewCard({review}){
+
+    return(
+        <div className="bar-review-card">
+            <div className="review-author">{review.user_id}</div>
+            <div className="review-rating">{review.star_rating}</div>
+            <body className="review-body">{review.content}</body>            
         </div>
     )
 }
--- baro-project/src/Home.jsx
+++ baro-project/src/Home.jsx
@@ -1,12 +1,30 @@
+import { useState,  useEffect } from "react"
 import { useNavigate } from "react-router-dom"
 
-export default function Home (){
-    const navigate = useNavigate()
 
+export default function Home ({setClickedBar}){
+    const navigate = useNavigate()
+    const [barArray, setBarArray] = useState([])
+
+
+    //fetch bars list
+    const fetchBars = async () => {
+        const req = await fetch('http://localhost:9292/bars')
+        const res = await req.json()
+        setBarArray(res)
+    }
+    useEffect(() => {
+        fetchBars()
+    }, [])  
+
+     
+
+    
 
 
     return(
         <div>
+            {/* Test Buttons */}
             <h1>Home Page</h1>
             <button type="button" onClick={() => navigate('/')}> Sign Out</button>
             <button type="button" onClick={() => navigate('/about')}> About</button>
@@ -16,8 +34,60 @@ export default function Home (){
             <button type="button" onClick={() => navigate('/crawllist')}> View all Crawls</button>
 
 
+            {/* display the list of bars  */}
+            <div className="bar-container">
+                {barArray.map((bar) => {
+                    return(
+                        <BarCard
+                            setClickedBar={setClickedBar}
+                            key={bar.id}
+                            bar={bar}                            
+                        />
+                    )
+                })}
+            </div>
+        </div>
+    )
+}
 
 
+
+function BarCard({setClickedBar, bar}) {
+    const navigate = useNavigate()
+    //states to hold the opacity if the cards when the mouse goes over them
+    const [mouseOverImage, setMouseOverImage] = useState(1)
+    const [mouseOverInfo, setMouseOverInfo] = useState(0)
+
+    return (
+        <div 
+            className="bar-card"
+            // fades and unfades the bar image and text when the mouse hovers over it
+            onMouseOver={()=>(
+                setMouseOverImage(.3),
+                setMouseOverInfo(1)
+            )}
+            onMouseLeave={()=>(
+                setMouseOverImage(1),
+                setMouseOverInfo(0)
+            )}
+             onClick={() => (
+                setClickedBar(bar),
+                navigate('/barinfo')
+                )}
+            >
+            {/* show this ifo when we mouse over the bar image */}
+            <div className="bar-card-info" style={{opacity: mouseOverInfo}}>
+                {bar.name}
+                {bar.review}
+                {bar.category}
+                {bar.price}
+            </div>
+            <img 
+                className="bar-card-image" 
+                src="https://www.shareicon.net/data/256x256/2016/11/16/854564_bar_512x512.png" 
+                alt={bar.name} 
+                style={{opacity: mouseOverImage}} 
+            />
         </div>
     )
 }
