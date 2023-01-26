e733ae258c265e7a175b277d7df3df3d3d3b5034
tomer <tomertal52@yahoo.com>
2023-01-26 15:04:16 -0500

    new css updates

--- baro-project/src/Home.jsx
+++ baro-project/src/Home.jsx
@@ -20,15 +20,22 @@ export default function Home ({setClickedBar}){
         fetchBars()
     }, [])  
 
+    //remove bar from bar-crawl list
+    // const removeBar = (barToRmove) => {
+    //     setCrawlArray(crawlArray.filter((bar) => {
+    //         return bar.id !== barToRmove.id
+    //     }))
+    // }
+
+
      
 
     
 
 
     return(
-        <div>
+        <div className="homepage">
             {/* Test Buttons */}
-            <h1 className="title">BarO</h1>
             <div className="nav-bar">
                 <button type="button" onClick={() => navigate('/')}> Sign Out</button>
                 <button type="button" onClick={() => navigate('/about')}> About</button>
@@ -36,21 +43,28 @@ export default function Home ({setClickedBar}){
                 <button type="button" onClick={() => navigate('/newcrawl')}> Create a Crawl</button>
                 <button type="button" onClick={() => navigate('/crawllist')}> View all Crawls</button>
             </div>
-            <div className="bar-crawl-container">
-                <div className="bar-crawl-list">
-                {crawlArray.map((bar) => {
-                    return(
-                        <BarCard
-                            type={'crawl'}
-                            setClickedBar={setClickedBar}
-                            key={bar.id}
-                            bar={bar}
-                            crawlArray={crawlArray}
-                            setCrawlArray={setCrawlArray}                            
-                        />
-                    )
-                })}
-                </div>
+            <img className="home-image" src="https://citizenside.com/wp-content/uploads/2022/12/bar-hopping-1-1170x780.jpg" />
+            <h1 className="title">BarO</h1>
+            <div className="welcome-message">
+            <h3>Welcome To BarO</h3>
+            <p className="description">The goal of BarO is to enhance your bar hopping experience to the MAX,</p>
+            <p className= "description">so you will have a night to remember!</p>
+            </div>
+                <div className="bar-crawl-container">
+                    <div className="bar-crawl-list">
+                    {crawlArray.map((bar) => {
+                        return(
+                            <BarCard
+                                type={'crawl'}
+                                setClickedBar={setClickedBar}
+                                key={bar.id}
+                                bar={bar}
+                                crawlArray={crawlArray}
+                                setCrawlArray={setCrawlArray}                            
+                            />
+                        )
+                    })}
+                    </div>
                 <button className="bar-crawl-button"
                     onClick={() => setCrawlArray([])}
                 >Clear Crawl</button>
--- baro-project/src/LoginPage.jsx
+++ baro-project/src/LoginPage.jsx
@@ -1,40 +1,53 @@
 import { useNavigate } from "react-router-dom";
-;
+import { useState } from "react";
 
 
 
 export default function LoginPage (){
     const navigate = useNavigate()
 
+    const [toggleLogin,  setToggleLogin] = useState(false);
+    
+    const handleToggle = () => {
+        setToggleLogin(!toggleLogin);
+    }
 
     return(
-
         <div className="login-background">
             <div className="login-div">
-                <h1>Welcome to BarO</h1>   
-                <form >
-                    <h3>Please Login</h3> 
-                    <input
-                        type="text"
-                        name="username"
-                        placeholder="User Name"
-                    /> 
-                    <br/>
-                     <input
-                        type="text"
-                        name="password"
-                        placeholder="Password"
-                    /> 
-                    <br/>
-                    <input
-                        type="submit"
-                        name="submit"
-                        className="submit"
-                    />
-                </form>
-                <button type="button" onClick={() => navigate('/home')}> Create and Account</button>
-                <br/>
-                <button type="button" onClick={() => navigate('/home')}> Continue as Guest</button>
+                <h1 className="welcome">BarO</h1>
+                <h2 className="motto">Bar Hoping Made Easy</h2>
+                {/*<img src="" />*/}
+                <button className="start-btn" onClick={handleToggle}>Begin Hopping</button>
+                { toggleLogin ? <div className="form-popup">
+                    <div className="form-div">
+                        <form className="login-form">
+                        <button className="exit-form" onClick={handleToggle}>End Your Journey</button>
+                        <h3>Please Login</h3> 
+                        <input
+                            type="text"
+                            name="username"
+                            placeholder="User Name"
+                        /> 
+                        <br/>
+                        <input
+                            type="text"
+                            name="password"
+                            placeholder="Password"
+                        /> 
+                        <br/>
+                        <input
+                            type="submit"
+                            name="submit"
+                            className="submit"
+                        />
+                        <br/>
+                        <button type="button" onClick={() => navigate('/home')}> Create an Account</button>
+                        <br/>
+                        <button type="button" onClick={() => navigate('/home')}> Continue as Guest</button>
+                        </form> 
+                    </div>
+                </div>: null}
 
 
                 
